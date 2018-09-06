3491d2003258814160ff2ed6a0d160e381c72ffd
Tricker_5 <tricker_5@localhost.localdomain>
2018-09-06 09:20:35 +0800

    no run 1.0.0

---
+++ src/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WSM;
+
+class Config{
+    const DBHOST = "10.0.2.2";
+    const DBPORT = "1433";
+    const DBNAME = "";
+    const DBUNAME = "sa";
+    const DBUPWD = "abc123456";
+
+    const WSIP = "0.0.0.0";
+    const WSPORT = 7777;
+}
+
+
+?>
